8d8be2f18243ba10649a83dc475d7a118ca15df6
Aaron Cochran <cochran.420@buckeyemail.osu.edu>
2020-04-10 16:56:24 -0400

    index.css Morning Psalm formatting index.html New Morning Psalm readscripture.js Attempting a web scraper

---
+++ readscripture.js
@@ -0,0 +1,22 @@
+var theURL = "https://www.presbyterianmission.org/devotion/daily/2020/4/10/#morning";
+
+window.onload = function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();
+}
