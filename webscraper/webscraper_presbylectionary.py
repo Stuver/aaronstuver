c04a9f22ab0978d386c00b47cfa3ea8066bf1a19
Aaron Cochran <cochran.420@buckeyemail.osu.edu>
2020-04-13 16:33:04 -0400

    Clean up presby scraper

--- webscraper/webscraper_presbylectionary.py
+++ webscraper/webscraper_presbylectionary.py
@@ -11,14 +11,6 @@ url = 'https://www.presbyterianmission.org/devotion/daily/' + datestr
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-# print (content)
-#
-# tweet = content.find('p', attrs={"class": "content"}).text
-# print (tweet)
-#
-# for tweet in content.find('p', attrs={"class": "content"}):
-#     print (tweet.text.encode('utf-8'))
-
 readingArr = []
 
 for reading in content.findAll('div', attrs={"class": "psalm"}):
@@ -38,38 +30,3 @@ for reading in content.findAll('div', attrs={"class": "reading"}):
     filename = ''.join(filename)
     print(filename)
     print(reading, file=open(filename, "w"))
-
-
-
-    # readingObject = {
-    # # "time": reading.find('a'),
-    # # "passage": reading.find('p', attrs={"class": "kappa"}).encode('utf-8'),
-    # # "text": reading.find('div').encode('utf-8')
-    # # reading.find('div', attrs={"class": "reading"})
-    # }
-    # passagetime = reading.find('a').encode('utf-8'),
-    # ''.join(passagetime),
-    # passagetime = passagetime.replace("<a name=\"", '', 1),
-
-
-
-    # readingObject = content.findAll('div', attrs={"class": "reading"})
-    # print (readingObject, file=open("scriptureData.json", "w"))
-
-#     readingArr.append(readingObject)
-# with open('scriptureData.json', 'w') as outfile:
-#     json.dump(readingArr, outfile)
-
-#     break
-#
-# for psalm in content.findAll('div', attrs={"class": "psalm"}):
-#     psalmObject = {
-#     "time": psalm.find('a'),
-#     "passage": psalm.find('p', attrs={"class": "kappa"}),
-#     "text": psalm.find('div')
-#     }
-#     readingArr.append(psalmObject)
-#     break
-
-
-    # print (psalmObject)
